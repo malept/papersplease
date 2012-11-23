4a59bc836cc16946edc88a687f93de73e9d28f8c
Mark Lee <papersplease@lazymalevolence.com>
2012-11-22 19:08:19 -0800

    Auto commit by dir2git

---
+++ papersplease/tests/test_certificates.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+class CertificateTestCase(unittest.TestCase):
+    '''Tests identity certs.'''
+
+    def test_generate_identity_cert(self):
+        '''
+        TODO: test by comparing the dict.
+        '''
+
+    def test_signed_identity_cert(self):
+        '''
+        TODO: test by verifying the signature.
+        '''
+
+    def test_rsa_jws(self):
+        '''
+        TODO: http://self-issued.info/docs/draft-ietf-jose-json-web-signature.html#RSASHA256Example
+        '''
